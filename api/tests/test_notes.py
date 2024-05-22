6c9e3d5a3931a5eaa8191b46f4f85bce7f96e02e
C. <cairohyfr@gmail.com>
2024-05-22 02:55:34 -0500

    test(backend): initial test for uploading notes

--- api/tests/test_notes.py
+++ api/tests/test_notes.py
@@ -1,58 +1,17 @@
-import os
-import pytest
+
 from flask import url_for
+
 from werkzeug.datastructures import FileStorage
 from io import BytesIO
 
-''' This test is meant to test when the uploads their notes,
- so currently at the moment I will only let 
- the user either upload .doc, ,docx, pdf, ppt and mp4.
- I feel like that's enough at the moment....but yes this works!'''
 
-def test_upload_pdf(client):
+
+
+def test_upload_file(client, mock_mongo):
     data = {
-        'file': (BytesIO(b'Mock PDF content'),'test.pdf'),
+        'file': (BytesIO(b"Hello World"), 'test.txt')
     }
-    response = client.post(url_for('upload_file'),content_type='multipart/form-data',data=data)
+    response = client.post(url_for('notes.upload_file'), content_type='multipart/form-data', data=data)
     assert response.status_code == 200
-    assert 'File uploaded sucessfully' in response.data.decode()
 
-def test_upload_doc(client):
-    data = {
-        'file': (BytesIO(b'Mock DOC content'),'test.doc'),
-    }
-    response = client.post(url_for('upload_file'),content_type='multipart/form-data',data=data)
-    assert response.status_code == 200
-    assert 'File uploaded sucessfully' in response.data.decode()
-
-def test_upload_docx(client):
-    data = {
-        'file': (BytesIO(b'Mock DOCX content'),'test.docx'),
-    }
-    response = client.post(url_for('upload_file'),content_type='multipart/form-data',data=data)
-    assert response.status_code == 200
-    assert 'File uploaded sucessfully' in response.data.decode()
-
-def test_upload_ppt(client):
-    data = {
-        'file': (BytesIO(b'Mock PPT content'),'test.ppt'),
-    }
-    response = client.post(url_for('upload_file'),content_type='multipart/form-data',data=data)
-    assert response.status_code == 200
-    assert 'File uploaded sucessfully' in response.data.decode()
-
-def test_upload_video(client):
-    data = {
-        'file': (BytesIO(b'Mock video content'), 'test.mp4'),
-    }
-    response = client.post(url_for('upload_file'), content_type='multipart/form-data', data=data)
-    assert response.status_code == 200
-    assert 'File uploaded successfully' in response.data.decode()
-
-def test_upload_unsupported_file_type(client):
-    data = {
-        'file': (BytesIO(b'Mock content'),'test.abc'),
-    }
-    response = client.post(url_for('upload_file'), content_type='multipart/form-data', data=data)
-    assert response.status_code == 400
-    assert 'Unsupported file type' in response.data.decode()
+        

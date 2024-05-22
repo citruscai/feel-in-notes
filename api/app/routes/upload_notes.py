87d23090ca259c0b5a921becc10ab2ed53f09197
C. <cairohyfr@gmail.com>
2024-05-22 02:55:03 -0500

    feat(backend): save uploaded note and generated worksheet into DB

--- api/app/routes/upload_notes.py
+++ api/app/routes/upload_notes.py
@@ -2,8 +2,9 @@ from flask import Blueprint,request,jsonify
 import textract
 from ..utils.process_notes import process_text
 import uuid
-import datetime
-from ..db import get_notes_collection,get_worksheets_collection
+from datetime import datetime
+from app.db import mongo
+
 
 
 notes_blueprint = Blueprint('notes',__name__)
@@ -12,25 +13,29 @@ notes_blueprint = Blueprint('notes',__name__)
 
 @notes_blueprint.route('/upload', methods=['POST'])
 def upload_file():
-
+    print("Received request with files:", request.files)
     if 'file' not in request.files:
-        return jsonify({'error':'No file uploaded'}),400
+        print("Error: No file part in the request")
+        return jsonify({'error': 'No file uploaded'}), 400
     file = request.files['file']
     if file.filename == '':
-        return jsonify({'error':'No file selected'}),400
-    notes_id = uuid.uuid4()
-    notes_collection = get_notes_collection()
-    worksheet_collection = get_worksheets_collection()
-    try:
-        text = extract_text(file)
-        notes_collection.insert_one({'id':notes_id,'text':text,'date_created': datetime.utcnow()})
-        processed_text = process_text(text)
-        worksheet_collection.insert_one({'id':notes_id, 'text':processed_text, 'date_created': datetime.utcnow()})
-        return jsonify({'text':processed_text}),200
-    except Exception as e:
-        return jsonify({'error':str(e)}),400
+        print("Error: No file selected")
+        return jsonify({'error': 'No file selected'}), 400
     
-       
+    try:
+        notes_id = uuid.uuid4()
+        level = request.form.get('level')
+        text = extract_text(file)
+        notes_collection = mongo.db.notes
+        worksheet_collection = mongo.db.worksheets
+        notes_collection.insert_one({'id': notes_id, 'text': text, 'date_created': datetime.now()})
+        processed_text = process_text(text,level)
+        worksheet_collection.insert_one({'id': notes_id, 'text': processed_text, 'date_created': datetime.now()})
+        return jsonify({'text': processed_text}), 200
+    except Exception as e:
+        print(f"Server error: {str(e)}")
+        return jsonify({'error': str(e)}), 400
+
 
 #using textract to extract the text from the uploaded file the user submits
 def extract_text(file):

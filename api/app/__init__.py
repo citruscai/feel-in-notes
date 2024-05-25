865d10632f2cc48bd21404366e6795cb1ef5c886
C. <cairohyfr@gmail.com>
2024-05-25 16:08:04 -0500

    feat(backend):enable cors

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+from flask_cors import CORS
 from config import Config  
 from .db import init_db
 from .routes.upload_notes import notes_blueprint
@@ -7,7 +7,7 @@ from .routes.upload_notes import notes_blueprint
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class) 
-    print("MongoDB URI: ", app.config['MONGO_URI'])
+    CORS(app)
 
     init_db(app)
    

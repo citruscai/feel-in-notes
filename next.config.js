6173b1ed7ca76a024db048143e4ebd8343e356da
C. <cairohyfr@gmail.com>
2024-06-01 19:11:03 -0500

    fix(frontend): fix next configuration for api use

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
-      },
-    ]
-  },
-}
 
-module.exports = nextConfig
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('http2', 'tls', 'net');
+    }
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      http2: false,
+      tls: false,
+      net: false,
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
